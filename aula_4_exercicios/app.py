d6b067b9814a4daf9eb219c000c15a9508b4dc89
Elisama Talita Cabral Santana <82914088+TalitaCbral@users.noreply.github.com>
2021-06-10 20:57:09 -0300

    ok

---
+++ aula_4_exercicios/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from app_blueprint import app_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(app_blueprint)
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
+
